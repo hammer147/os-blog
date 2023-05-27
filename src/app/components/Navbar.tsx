d93eaca3351940a0858346e4c2f957315367d3c1
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2023-05-27 13:17:24 +0200

    updated links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from 'react-icons/fa'
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop, FaMicrosoft } from 'react-icons/fa'
 
 export default function Navbar() {
   return (
@@ -11,18 +11,18 @@ export default function Navbar() {
           </Link>
         </h1>
         <div className='flex flex-row justify-center gap-4 align-middle text-4xl text-white sm:justify-evenly lg:text-5xl'>
+          <Link className='text-white/90 hover:text-white' href='https://learn.microsoft.com/en-us/office/dev/scripts/overview/excel'>
+            <FaMicrosoft />
+          </Link>
           <Link
             className='text-white/90 hover:text-white'
-            href='https://www.youtube.com/'>
+            href='https://www.youtube.com/results?search_query=office+scripts'>
             <FaYoutube />
           </Link>
-          <Link className='text-white/90 hover:text-white' href='https://udemy.com/'>
-            <FaLaptop />
-          </Link>
-          <Link className='text-white/90 hover:text-white' href='https://github.com/'>
+          <Link className='text-white/90 hover:text-white' href='https://github.com/search?q=officescripts&type=repositories'>
             <FaGithub />
           </Link>
-          <Link className='text-white/90 hover:text-white' href='https://twitter.com/'>
+          <Link className='text-white/90 hover:text-white' href='https://twitter.com/hashtag/OfficeScripts'>
             <FaTwitter />
           </Link>
         </div>
