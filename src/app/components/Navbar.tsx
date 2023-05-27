d9ad585bd47827a50b2462c6edd33ab2761c73b2
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2023-05-27 13:25:29 +0200

    added target _blank to external links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,25 +4,35 @@ import { FaYoutube, FaTwitter, FaGithub, FaLaptop, FaMicrosoft } from 'react-ico
 export default function Navbar() {
   return (
     <nav className='sticky top-0 z-10 bg-slate-600 p-4 drop-shadow-xl'>
-      <div className='max-w-5xl mx-auto flex flex-col justify-between sm:flex-row md:px-6'>
+      <div className='mx-auto flex max-w-5xl flex-col justify-between sm:flex-row md:px-6'>
         <h1 className='mb-2 grid place-content-center text-3xl font-bold text-white md:mb-0'>
           <Link href='/' className='text-white/90 no-underline hover:text-white'>
             Office Scripts Blog
           </Link>
         </h1>
         <div className='flex flex-row justify-center gap-4 align-middle text-4xl text-white sm:justify-evenly lg:text-5xl'>
-          <Link className='text-white/90 hover:text-white' href='https://learn.microsoft.com/en-us/office/dev/scripts/overview/excel'>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://learn.microsoft.com/en-us/office/dev/scripts/overview/excel'
+            target='_blank'>
             <FaMicrosoft />
           </Link>
           <Link
             className='text-white/90 hover:text-white'
-            href='https://www.youtube.com/results?search_query=office+scripts'>
+            href='https://www.youtube.com/results?search_query=office+scripts'
+            target='_blank'>
             <FaYoutube />
           </Link>
-          <Link className='text-white/90 hover:text-white' href='https://github.com/search?q=officescripts&type=repositories'>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://github.com/search?q=officescripts&type=repositories'
+            target='_blank'>
             <FaGithub />
           </Link>
-          <Link className='text-white/90 hover:text-white' href='https://twitter.com/hashtag/OfficeScripts'>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://twitter.com/hashtag/OfficeScripts'
+            target='_blank'>
             <FaTwitter />
           </Link>
         </div>
