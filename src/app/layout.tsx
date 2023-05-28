150028364fa0f4632c7435e13a8fe038d1e1f699
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2023-05-28 11:24:39 +0200

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from './components/Navbar'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: 'Office Scripts Blog',
@@ -10,11 +11,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className='dark:bg-slate-800 min-h-screen'>
+      <body className='min-h-screen dark:bg-slate-800'>
         <Navbar />
-        <main className='max-w-5xl prose prose-xl prose-slate dark:prose-invert mx-auto px-4 md:px-6'>
+        <main className='prose prose-xl prose-slate mx-auto max-w-5xl px-4 dark:prose-invert md:px-6'>
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   )
